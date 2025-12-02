59a34ee0878dcfb6451e81804ad59a8b14ef1b4a
tincho1980 <miramallo@gmail.com>
2025-12-02 15:47:35 -0300

    getAll de sedes ok con serialize

--- fines_heat_map/fines_heat_map/settings.py
+++ fines_heat_map/fines_heat_map/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "heatmap",
     "gestion_instituciones",
     "gestion_comisiones",
--- fines_heat_map/heatmap/views.py
+++ fines_heat_map/heatmap/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from gestion_instituciones.models import Sede
+from gestion_instituciones.serializers import SedeCompletaSerializer
 
-
-# Create your views here.
 def mapa(request):
     return render(request, "heatmap/mapa.html")
+
+
+def get_sedes_data(request):
+    # Endpoint para obtener los datos de las sedes con filtros de turno, orientación y módulo.
+    sedes = (
+        Sede.objects.filter(deleted_at=None)
+        .select_related('localidad', 'localidad__partido', 'sede_tipo')
+        .prefetch_related('comisiones', 'comisiones__orientacion', 'comisiones__modulo')
+    )
+
+    serializer = SedeCompletaSerializer(sedes, many=True)
+
+    return JsonResponse(serializer.data, safe=False)
