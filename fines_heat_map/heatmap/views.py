4cf6a4e49846f12df93708091add1c2c06d6b9a5
Tati <santiagomatheu@gmail.com>
2025-12-11 18:59:37 -0300

    rebase de develop

--- fines_heat_map/heatmap/views.py
+++ fines_heat_map/heatmap/views.py
@@ -80,7 +80,7 @@ def mapa(request):
         "show_heatmap": show_heatmap,
         "map_config": settings.MAP_CONFIG,
     }
-    return render(request, "home. html", context)
+    return render(request, "home.html", context)
 
 
 def get_sedes_data(request):
