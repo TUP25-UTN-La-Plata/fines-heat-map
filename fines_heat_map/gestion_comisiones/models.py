d2061e5f83ebe852e50fabb978a716d84a5f5b2a
tincho1980 <miramallo@gmail.com>
2025-12-12 14:24:01 -0300

    admin ok

--- fines_heat_map/gestion_comisiones/models.py
+++ fines_heat_map/gestion_comisiones/models.py
@@ -37,12 +37,13 @@ class Comision(models.Model):
         TARDE = 'T', 'Tarde'
         VESPERTINO = 'V', 'Vespertino'
         NOCHE = 'N', 'Noche'
+        INTERMEDIO = 'I', 'Intermedio'
 
     sede = models.ForeignKey(Sede, on_delete=models.RESTRICT, related_name='comisiones')
     orientacion = models.ForeignKey(Orientacion, on_delete=models.RESTRICT, related_name='comisiones')
     modulo = models.ForeignKey(Modulo, on_delete=models.RESTRICT, related_name='comisiones')
     numero = models.CharField(max_length=50, help_text="Código único de la comisión (Ej: 2024-LP-01)")
-    horario = models.CharField(max_length=100, help_text="Días y horarios")
+    horario = models.CharField(max_length=100, help_text="Días y horarios", blank=True, null=True)
     turno = models.CharField(max_length=1, choices=Turno.choices)
     tutor = models.CharField(max_length=200, blank=True, null=True)
     notas = models.TextField(blank=True, null=True)
